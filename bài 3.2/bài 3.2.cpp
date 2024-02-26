517362aede945a6f18b7297d6b264b897d8df2b9
dinhnguyencntt <dn1994099@gmail.com>
2024-02-26 09:19:40 +0700

    Add project files.

---
+++ bài 3.2/bài 3.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "chuong trinh tính tong\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << "=" << tong << endl;
+	return 0;
+}
